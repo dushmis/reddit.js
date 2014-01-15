003be421e90b965b560ae4167cf5864390a65e84
dushyant <dushyant.mistry@youbroadband.co.in>
2014-01-15 16:37:04 +0000

    next method and class

--- rdata.js
+++ rdata.js
@@ -1,17 +1,17 @@
 
 function Rdata (rdata) {
-	this.data=rdata;
+  this.data=rdata;
 }
 
 
 Rdata.prototype.next = function(callback) {
-			if (typeof callback === "function") {
-				(new reddit({limit:this.settings.limit,next:this.data.after})).get(this.settings.what, function(err,data) {
-					if (!err) {
-						 callback(undefined, data);
-					};
-				});	
-			};
+      if (typeof callback === "function") {
+        (new reddit({limit:this.settings.limit,next:this.data.after})).get(this.settings.what, function(err,data) {
+          if (!err) {
+             callback(undefined, data);
+          };
+        }); 
+      };
 };
 
 module.exports = Rdata;
--- test.js
+++ test.js
@@ -1,29 +1,19 @@
 var reddit = require("./reddit");
 var cons = require("./cons");
 
-<<<<<<< HEAD
 var acallback=function(err,data) {
-	if (!err) {
-		displayContent(data.data.children);
-		data.next(acallback);
-	};
+  if (!err) {
+    displayContent(data.data.children);
+    data.next(acallback);
+  };
 };
 
 var displayContent = function(children){
-	for (var i = children.length - 1; i >= 0; i--) {
-		console.log(children[i].data.title);
-		//console.log(children[i].data.url);
-	};
+  for (var i = children.length - 1; i >= 0; i--) {
+    console.log(children[i].data.title);
+    //console.log(children[i].data.url);
+  };
 };
 
 (new reddit({limit:5})).get("stories", acallback);
 //console.log(cons);
-=======
-(new reddit({limit:5})).get("stories", function(err,data) {
-	if (err) {
-		console.log(err);
-	}else{
-		console.log(data);
-	}
-});
->>>>>>> d72d6a56cc8baa8368a01e5726e33bb3014164d5
