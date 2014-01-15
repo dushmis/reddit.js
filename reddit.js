0c261055e15767508c68f3f151f946aebf9f1b9d
dushyant <dushyant.mistry@youbroadband.co.in>
2014-01-15 10:57:29 +0000

    error handling

--- reddit.js
+++ reddit.js
@@ -1,59 +1,51 @@
 (function() {
-var http = require('http');
-var express = require("express");
-var logfmt = require("logfmt");
-var app = express();
-var default_settings={
-  limit:5
-};
-  reddit = (function() {
+  var http = require("http");
+  var express = require("express");
+  var logfmt = require("logfmt");
+  var app = express();
+  var default_settings = {limit:5};
+  reddit = function() {
     function reddit(settings) {
-      default_settings.limit = settings.limit?settings.limit:default_settings.limit;
+      default_settings.limit = settings.limit ? settings.limit : default_settings.limit;
       default_settings.next = settings.next;
     }
-    
-    reddit.prototype.get = function(url,callback){
-      url="http://www.reddit.com/r/"+url+"/.json";
-      if(default_settings.limit){
-        url=url+"?limit="+default_settings.limit;
+    reddit.prototype.get = function(url, callback) {
+      url = "http://www.reddit.com/r/" + url + "/.json";
+      if (default_settings.limit) {
+        url = url + "?limit=" + default_settings.limit;
       }
-      if(default_settings.next){
-        url=url+"&after="+default_settings.next;
+      if (default_settings.next) {
+        url = url + "&after=" + default_settings.next;
       }
-      //console.log(url);
-      //console.log("default_settings = " + default_settings.next);
-      if( typeof url === 'undefined' ){
+      if (typeof url === "undefined") {
         return undefined;
       }
       http.get(url, function(res) {
-        var body = '';
-        res.on('data', function(chunk) {
+        var body = "";
+        res.on("data", function(chunk) {
           body += chunk;
         });
-        res.on('end', function() {
-          //console.log(res);
-          if(res.statusCode=="200"){
-          var resp = JSON.parse(body)
-            if(! (typeof callback === 'undefined') && (typeof callback==="function")){
-              if(resp && resp.data){
-                callback(undefined,resp.data);
+        res.on("end", function() {
+          if (res.statusCode == "200") {
+            var resp = JSON.parse(body);
+            if (!(typeof callback === "undefined") && typeof callback === "function") {
+              if (resp && resp.data) {
+                callback(undefined, resp.data);
               }
-            }else{
+            } else {
               console.log("pass something");
             }
-          }else{
-             if(! (typeof callback === 'undefined') && (typeof callback==="function")){
-                callback({message:"Undefined location"},undefined);
+          } else {
+            if (!(typeof callback === "undefined") && typeof callback === "function") {
+              callback({message:"Undefined location"}, undefined);
             }
           }
         });
-      }).on('error', function(e) {
-          callback(e,undefined);
+      }).on("error", function(e) {
+        callback(e, undefined);
       });
-    }
+    };
     return reddit;
-  })();
-  
+  }();
   module.exports = reddit;
-  
 }).call(this);
