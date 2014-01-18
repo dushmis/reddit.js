267c523d2731c2acb5f1b9ec3727a8c9931c597a
EC2 Default User <ec2-user@ip-172-31-27-99.ap-southeast-1.compute.internal>
2014-01-18 14:55:40 +0530

    test cases for comments

--- test.js
+++ test.js
@@ -1,19 +1,53 @@
-var reddit = require("./reddit");
-var cons = require("./cons");
+#!/usr/local/bin/node
 
-var acallback=function(err,data) {
+var reddit = require("./reddit");
+all=[];
+var acallback = function(err, data) {
   if (!err) {
     displayContent(data.data.children);
-    data.next(acallback);
-  };
+  }
 };
 
-var displayContent = function(children){
-  for (var i = children.length - 1; i >= 0; i--) {
-    console.log(children[i].data.title);
-    //console.log(children[i].data.url);
-  };
+var displayContent = function(children) {
+  children.forEach(function(result) {
+    title = result.data.title || {};
+    result.data.permalink.read(function(err, data) {
+      data.data[1].data.children.forEach(function(result) {
+        if(result.data && result.data.body && result.data.body.length>20 && result.data.body.length<140 && isValid(result.data.body)){
+          //console.log(result.data.body);
+          all.push(result.data.body);
+        }
+      });
+    });
+  });
+};
+var args = process.argv.splice(2);
+
+var rand_Index = Math.round((Math.random()*args.length-1));
+rand_Index=rand_Index==-1?0:rand_Index;
+
+var nag = ["http","@","www","Edit"];
+
+var isValid=function(t){
+  for(var i in nag){
+    n=nag[i].toLowerCase();
+    if(t.toLowerCase().indexOf(n)!=-1){
+      return false;
+    }
+  }
+  return true;
 };
 
-(new reddit({limit:5})).get("stories", acallback);
-//console.log(cons);
+//console.log("will search for "+args[rand_Index]);
+
+(new reddit({limit:1})).get(args[rand_Index], acallback);
+
+process.on('exit', function() {
+  var rand_Index = Math.round((Math.random()*all.length-1));
+  rand_Index=rand_Index==-1?0:rand_Index;
+  if(all[rand_Index]){
+    console.log(all[rand_Index]);
+  }
+});
+
+
